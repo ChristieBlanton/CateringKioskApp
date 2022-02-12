4792269c9874070b00eb2639f03432636adaf82f
ChristieBlanton <94931388+ChristieBlanton@users.noreply.github.com>
2022-02-12 10:44:58 -0500

    Save Capstone progress

--- 19_Mini-Capstone/CapstoneTests/CateringTest.cs
+++ 19_Mini-Capstone/CapstoneTests/CateringTest.cs
@@ -19,29 +19,7 @@ namespace CapstoneTests
             //Assert
             Assert.IsNotNull(catering);
         }
-        [TestMethod]
-        public void SubtractMoneyTest()
-        {
-            Money money = new Money();
-            money.AddMoney("100");
-            money.SubtractMoney(50m);
-
-            Assert.AreEqual(50, money.Balance);
-
-
-        }
-        [TestMethod]
-        public void MakeChangeTest()
-        {
-            Money money = new Money();
-            Dictionary<string, int> test = new Dictionary<string, int>();
-            test.Add("Fifties", 4 );
-            test.Add("Twenties", 2);
-
-            Dictionary<string, int> result = money.MakeChange(240);
-
-            CollectionAssert.AreEquivalent(test, result);
-        }
+        
     }
 
     
---
+++ 19_Mini-Capstone/CapstoneTests/MoneyTests.cs
@@ -0,0 +1,36 @@
+﻿using Capstone;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class MoneyTests
+    {
+        [TestMethod]
+        public void SubtractMoneyTest()
+        {
+            Money money = new Money();
+            money.AddMoney("100");
+            money.SubtractMoney(50m);
+
+            Assert.AreEqual(50, money.Balance);
+
+
+        }
+        [TestMethod]
+        public void MakeChangeTest()
+        {
+            Capstone.Money money = new Money();
+            Dictionary<string, int> test = new Dictionary<string, int>();
+            test.Add("Fifties", 4);
+            test.Add("Twenties", 2);
+
+            Dictionary<string, int> result = money.MakeChange(240);
+
+            CollectionAssert.AreEquivalent(test, result);
+        }
+    }
+}
